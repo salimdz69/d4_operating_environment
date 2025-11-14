97e94bd582c65137f2997255a82046102fa88da2
salimdz69 <salimdzsalim12@gmail.com>
2025-11-14 21:56:07 +0100

    Initial commit

---
+++ sysexit.py
@@ -0,0 +1,8 @@
+# | 'sysexit' |
+# 'sysexit' is a built-in command for the 'd4' operating environment project to stop and exit from 'd4'.
+# 'sysexit' comes with no license and it's totally free to use and modify, use it and it's source code wisely.
+# 'sysexit' is in a stable development state, but expect changes in further updates.
+
+# Main functionality :
+def main():
+    quit()
---
+++ syswlcm.py
@@ -0,0 +1,14 @@
+# | 'syswlcm' |
+# 'syswlcm' is a built-in function for the 'd4' operating environment project to show help tips for the user at start-up.
+# 'syswlcm' comes with no license and it's totally free to use and modify, use it and it's source code wisely.
+# 'syswlcm' is in a stable development state, but expect changes in further updates.
+
+# imports 'smoldes' from 'sysdtls.py' :
+from sysdtls import smoldes
+
+# main functionality :
+def main():
+    print("> | d4 Operating enviromment |")
+    print(">", smoldes)
+    print("> Run 'sysco' to see avaliable commands")
+    print("> Run 'syscrdt' to see credits of d4 OE")
